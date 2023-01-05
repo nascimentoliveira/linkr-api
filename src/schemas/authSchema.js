d1d5bd1bf7aad02881037721dd5eb4fdf7219ccc
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-05 18:49:17 -0300

    refact: url on auth schema (picture)

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -1,16 +1,13 @@
 import joi from "joi";
 
 export const signUpSchema = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().required(),
-    username: joi.string().required(),
-    picture: joi
-        .string()
-        .pattern(new RegExp(".(jpg|jpeg|png|webp|avif|gif|svg)$"))
-        .required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+  username: joi.string().required(),
+  picture: joi.string().uri().trim().required(),
 });
 
 export const signInSchema = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
 });
