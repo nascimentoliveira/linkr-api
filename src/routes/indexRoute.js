4c8af3dcbef18bf878c8952026128eccea10dc4e
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-05 18:54:56 -0300

    refact: merge from main (auth)

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
 import postsRoutes from './postsRoute.js';
-import authRouter from './authRouter.js'
-
 
 const router = Router();
 
-router.use(postsRoutes, authRouter);
+router.use(postsRoutes);
 
 export default router;
