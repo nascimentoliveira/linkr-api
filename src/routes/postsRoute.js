fcebcc9fa1e43f00422ad164762f7bf030cb379e
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-05 18:39:12 -0300

    refact: inicio pagina user

--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 
 import validateSchema from "../middlewares/schemaValidator.js";
 import postSchema from "../schemas/postSchema.js";
-import { fetchMetadata, newPost } from "../controllers/postController.js";
+import {
+  fetchMetadata,
+  fetchUserData,
+  newPost,
+} from "../controllers/postController.js";
 import { fetchData } from "../middlewares/postMiddleware.js";
 
 const router = Router();
@@ -10,5 +14,7 @@ const router = Router();
 router.post("/posts", validateSchema(postSchema), newPost);
 
 router.get("/timeline", fetchData, fetchMetadata);
+
+router.get("/user/:id", fetchUserData);
 
 export default router;
