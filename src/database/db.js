565262ba5938ffad202b1cc104d6f3cc77c5ee2f
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-06 23:51:37 -0300

    fix: db

--- src/database/db.js
+++ src/database/db.js
@@ -1,19 +1,14 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
+import pg from "pg";
+import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pg;
 
 const configDatabase = {
-  // connectionString: process.env.DATABASE_URL,
-  host: 'localhost',
-  port: 5432,
-  user:'postgres',
-  database:'linkr',
-  password:'91Dc001832//'
+  connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.MODE === 'prod') configDatabase.ssl = true;
+if (process.env.MODE === "prod") configDatabase.ssl = true;
 
 const db = new Pool(configDatabase);
 
