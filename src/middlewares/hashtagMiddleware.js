5abdd58355d5aff9aa20eb39656a930062069037
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-09 11:57:06 -0300

    fix: metadata bug

--- src/middlewares/hashtagMiddleware.js
+++ src/middlewares/hashtagMiddleware.js
@@ -24,3 +24,18 @@ export async function hashtagValid(req, res, next) {
 
   next();
 }
+
+export async function hashtagPosts(req, res, next) {
+
+  const { id } = res.locals.hashtagId;
+
+  try {
+    const hashtagPosts = (await hashtagRepository.getHashtagPosts(id)).rows;
+    res.locals.data = hashtagPosts;
+    next()
+
+  } catch (err) {
+    console.error(MESSAGES.INTERNAL_SERVER_ERROR, err);
+    res.status(500).send({ message: MESSAGES.CLIENT_SERVER_ERROR });
+  }
+}
