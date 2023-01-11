d252eb1bd1816f9764526155bb661692619a75c1
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-11 14:19:24 -0300

    search engine updated

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,14 +1,38 @@
-import searchUser from "../repositories/searchRepository.js";
 import { MESSAGES } from "../constants.js";
+import searchRepository from "../repositories/searchRepository.js";
 
-export default async function searchUsers(req,res){
-    const {search} = req.body;
-    try{
-        const {rows} = await searchUser(search)
-        res.status(200).send(rows)
-    } catch(err){
-        console.error(err)
-        res.status(500).send({message: MESSAGES.FETCH_USERS_ERROR})
-    }
+export async function searchUsers(req, res, next) {
+  const { search } = req.body;
+  const { id } = res.locals.user;
+  try {
+    const { rows } = await searchRepository.searchUser(search);
+    res.locals.data = rows;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: MESSAGES.FETCH_USERS_ERROR });
+  }
 }
 
+export async function checkSearchFollows(req, res) {
+  const { data } = res.locals;
+  const myId = res.locals.user.id;
+  try {
+    const treatedData = [];
+    for (let i = 0; i < data.length; i++) {
+      const { rows } = await searchRepository.checkFollow(myId, data[i].id);
+      if (rows.length > 0) {
+        treatedData.push({ ...data[i], follows: true });
+      } else {
+        treatedData.push({ ...data[i], follows: false });
+      }
+    }
+    treatedData.sort((a, b) => {
+      return b.follows - a.follows;
+    });
+    res.status(200).send(treatedData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: MESSAGES.FETCH_USERS_ERROR });
+  }
+}
--- src/repositories/searchRepository.js
+++ src/repositories/searchRepository.js
@@ -1,15 +1,27 @@
 import db from "../database/db.js";
 
-export default async function searchUser(search){
+async function searchUser(search){
     return db.query(`
-        SELECT 
-            picture,
-            username,
-            id
-        FROM 
-            users
-        WHERE username
-            ILIKE $1
-        ORDER BY username
+    SELECT 
+    picture,
+    username,
+    id
+FROM 
+    users 
+WHERE username
+    ILIKE $1
     `,[`${search}%`])
 }
+
+async function checkFollow(myId,id){
+    return db.query(`
+    SELECT * FROM followers WHERE "followerId" = $1 AND "followedId" = $2
+    `,[myId,id])
+}
+
+const searchRepository = {
+    searchUser,
+    checkFollow
+}
+
+ export default searchRepository
