f05112e10aad07f83bf29292f3d1a890dbeeae83
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-26 19:59:12 -0300

    feat: swagger docs

---
+++ src/docs.js
@@ -0,0 +1,19 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Shortly-API",
+      version: "1.0.0",
+      description: "API de Linkr, uma rede social que permite aos usuários compartilhar e descobrir links interessantes.",
+    },
+  },
+
+  apis: ["src/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+export default swaggerSpec;
+//
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
 
 import posts from "./posts-router.js";
 import auth from "./auth-router.js";
@@ -8,10 +9,12 @@ import search from "./search-router.js";
 import followers from "./followers-router.js";
 import comments from "./comments-router.js";
 import health from "./health-router.js";
+import swaggerSpec from "../docs.js";
 
 const api = Router();
 api
   .use("/", health)
+  .use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
   .use("/auth", auth)
   .use("/posts", posts)
   .use("/likes", likes)
