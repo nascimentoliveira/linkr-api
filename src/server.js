9099aef5f9f780dabc296cb39cfadac835c93a5a
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-26 09:57:25 -0300

    refactor: create server file

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import app from "./app";
+
+const port = +process.env.PORT || 4000;
+
+app.listen(port, function (err) {
+  if (err) {
+    /* eslint-disable-next-line no-console */
+    console.log("Failed to start the server -", err);
+  }
+  /* eslint-disable-next-line no-console */
+  console.log("Server listening on port", port);
+});
+//
