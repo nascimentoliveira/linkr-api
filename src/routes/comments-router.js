ec783b966cd9ae6d3a36bf58696138c582b1dec8
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-26 10:23:06 -0300

    refactor: comments router

--- src/routes/comments-router.js
+++ src/routes/comments-router.js
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { createComment, getComments } from "../controllers/commentsController.js";
 import { tokenValid } from "../middlewares/authMiddleware.js";
 
-const commentsRouter = Router();
+const comments = Router();
 
-commentsRouter.post("/comments",tokenValid,createComment);
-commentsRouter.get("/comments", tokenValid,getComments);
+comments
+  .all("/*", tokenValid)
+  .post("/", createComment)
+  .get("/", getComments);
 
-export default commentsRouter;
+export default comments;
+//
