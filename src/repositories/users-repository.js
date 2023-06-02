d410b3b73be1a2c62cb881907087017ef8767b05
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-06-02 18:23:58 -0300

    refactor: users repository

--- src/repositories/users-repository.js
+++ src/repositories/users-repository.js
@@ -1,86 +1,68 @@
 import connectionDB from "../database/database.js"
 
+async function getUserById(userId) {
+  return connectionDB.query(`
+    SELECT * 
+    FROM users 
+    WHERE id=$1;`,
+    [userId],
+  );
+}
+
 async function getUserByEmail(email) {
   return connectionDB.query(`
-    SELECT 
-      * 
-    FROM 
-      users 
-    WHERE 
-      email=$1;`,
+    SELECT * 
+    FROM users 
+    WHERE email=$1;`,
     [email],
   );
 }
 
 async function getUserBySession(sessionId) {
   return connectionDB.query(`
-    SELECT 
-      users.* 
-    FROM 
-      sessions
-    JOIN 
-      users
-    ON 
-      session."userId"=users.id
-    WHERE 
-      session.id=$1;`,
+    SELECT users.* 
+    FROM sessions
+    JOIN users
+      ON sessions."userId"=users.id
+    WHERE sessions.id=$1;`,
     [sessionId],
   );
 }
 
-async function createUser(name, email, password, picture) {
+async function createUser(username, email, password, picture) {
   return connectionDB.query(`
-    INSERT INTO 
-      users(username, email, password, picture)
-    VALUES 
-      ($1, $2, $3, $4);`,
-    [name, email, password, picture],
+    INSERT INTO users(username, email, password, picture)
+    VALUES ($1, $2, $3, $4);`,
+    [username, email, password, picture],
   );
 }
 
 async function createSession(userId) {
   return connectionDB.query(`
-    INSERT INTO 
-      sessions("userId")
-    VALUES 
-      ($1) 
-    RETURNING 
-      id;`,
+    INSERT INTO sessions("userId")
+    VALUES ($1) 
+    RETURNING id;`,
     [userId]
   );
 }
 
 async function searchUser(username) {
   return connectionDB.query(`
-    SELECT 
-      id, picture, username
-    FROM 
-      users 
+    SELECT id, picture, username
+    FROM users 
     WHERE username
     ILIKE $1;`,
     [`${username}%`],
   );
 }
 
-async function checkFollow(followerId, followedId) {
-  return connectionDB.query(`
-    SELECT 
-      * 
-    FROM 
-      followers 
-    WHERE 
-      "followerId"=$1 AND "followedId"=$2;`,
-    [followerId, followedId],
-  );
-}
-
 const usersRepository = {
+  getUserById,
   getUserByEmail,
   getUserBySession,
   createUser,
   createSession,
   searchUser,
-  checkFollow,
 };
 
 export default usersRepository;
