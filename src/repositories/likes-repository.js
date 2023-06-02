1ed726da5cd3a15a5b6d08b6ec5f3a605d72ac3e
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-06-02 18:24:47 -0300

    refactor: likes repository

--- src/repositories/likes-repository.js
+++ src/repositories/likes-repository.js
@@ -1,70 +1,56 @@
 import connectionDB from "../database/database.js"
 
-async function likePost(userId, postId) {
+async function like(userId, postId) {
   return connectionDB.query(`
-    INSERT INTO 
-      likes("userId", "postId") 
-    VALUES
-      ($1, $2);`,
+    INSERT INTO likes("userId", "postId")
+    VALUES ($1, $2);`,
     [userId, postId],
   );
 }
 
-async function dislikePost(userId, postId) {
+async function unlike(userId, postId) {
   return connectionDB.query(`
-    DELETE FROM 
-      likes 
+    DELETE FROM likes
     WHERE "userId"=$1 AND "postId"=$2;`,
     [userId, postId],
   );
 }
 
-async function countLikes(postId) {
+async function getLikes(userId, postId) {
   return connectionDB.query(`
-    SELECT 
-      COUNT (likes."postId") 
-    FROM 
-      likes 
-    WHERE 
-      "postId"=$1;`,
-    [postId],
+    SELECT
+      (SELECT COUNT(*) FROM likes WHERE "postId"=$2) AS likes,
+      EXISTS (
+        SELECT 1
+        FROM likes
+        WHERE "postId"=$2 AND "userId"=$1
+      ) AS liked,
+      COALESCE((
+        SELECT json_agg(users.username) 
+        FROM likes 
+        JOIN users 
+          ON likes."userId"=users.id 
+        WHERE likes."postId"=$2 AND likes."userId"<>$1 
+        LIMIT 3),'[]'::json
+      ) AS likers;`,
+    [userId, postId],
   );
 }
 
-async function getLikes() { // verificar
+async function checkLiked(userId, postId) {
   return connectionDB.query(`
-    SELECT 
-      * 
-    FROM 
-      likes;`,
-  );
-}
-
-async function getLikesId(id) {
-  return connectionDB.query(`
-    SELECT 
-      likes.id, 
-      likes."postId", 
-      users.username, 
-      likes."userId"
-    FROM 
-      likes
-    JOIN 
-      users 
-    ON 
-      users.id=likes."userId"
-    WHERE 
-      likes."postId" = $1;`,
-    [id],
+    SELECT *
+    FROM likes
+    WHERE likes."userId"=$1 AND likes."postId"=$2;`,
+    [userId, postId],
   );
 }
 
 const likesRepository = {
-  likePost,
-  dislikePost,
-  countLikes,
+  like,
+  unlike,
   getLikes,
-  getLikesId,
+  checkLiked,
 }
 
 export default likesRepository;
