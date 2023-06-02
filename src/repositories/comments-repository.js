d5bb48fe52b6a9e00c82fde2d81a9ba48850dd42
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-06-02 18:26:28 -0300

    refactor: comments repository

--- src/repositories/comments-repository.js
+++ src/repositories/comments-repository.js
@@ -2,41 +2,39 @@ import connectionDB from "../database/database.js"
 
 async function createComment(postId, userId, comment) {
   return connectionDB.query(`
-    INSERT INTO 
-      comments ("postId", "userId", comment)
-    VALUES 
-      ($1, $2, $3);`,
+    WITH insertedComment AS (
+      INSERT INTO comments ("postId", "userId", comment)
+      VALUES ($1, $2, $3)
+      RETURNING "postId"
+    )
+    SELECT json_agg(json_build_object(
+        'id', comments.id,
+        'username', users.username,
+        'picture', users.picture,
+        'userId', users.id,
+        'comment', comments.comment,
+        'isAuthor', (comments."userId"=posts."userId"),
+        'follow', EXISTS (
+          SELECT 1
+          FROM followers
+          WHERE followers."followerId"=$2
+          AND followers."followedId"=users.id
+        )
+      ) ORDER BY comments."createdAt"
+    ) AS comments
+    FROM comments
+    JOIN posts 
+      ON comments."postId"=posts.id
+    JOIN users 
+      ON comments."userId"=users.id
+    WHERE comments."postId"=(SELECT "postId" FROM insertedComment)
+    GROUP BY posts.id;`,
     [postId, userId, comment],
-  );
-};
-
-async function getComments(postId) {
-  return connectionDB.query(`
-    SELECT 
-      comments."userId" as "userIdComment", 
-      posts."userId" as "userIdPost", 
-      users.username, 
-      users.picture, 
-      comments.comment
-    FROM 
-      comments
-    JOIN 
-      users
-    ON 
-      comments."userId"=users.id
-    LEFT JOIN 
-      posts
-    ON 
-      comments."postId"=posts.id
-    WHERE 
-      comments."postId"=$1;`,
-    [postId],
   );
 };
 
 const commentsRepository = {
   createComment,
-  getComments,
 };
 
 export default commentsRepository;
